ca27c9099146a12dc1b332eaaef63167ec45a9bc
Zoki91 <zmitrevski2@gmail.com>
2022-06-19 13:34:37 +1000

    index page, scores and questions

---
+++ questions.js
@@ -0,0 +1,69 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
+    answer: "<script>"
+  },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "Both the <head> section and the <body> section are correct",
+      "The <body> section",
+      "The <head> section",
+      ],
+    answer: "The <body> section"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "alert('Hello World')",
+      "msgBox('Hello World')",
+      "alertBox('Hello World')",
+      "msg('Hello World')",
+    ],
+    answer: "alert('Hello World')"
+  },
+  {
+    title:
+      "If Button is clicked (.......) Event Handler is invoked.",
+    choices: [
+      "OnSubmit()",
+      "OnLoad()",
+      "IsPostBack()",
+      "Onclick()"
+    ],
+    answer: "Onclick()"
+  },
+  {
+    title: "What is the data type of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data type",
+      "None of the above",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+
+];
